2283ce282fa69f80a593f8d2cc44ed68dd59e5f4
smile <imsmilegarg@gmail.com>
2021-05-10 17:54:56 +0530

    home_screen.dart

--- lib/View/MainScreens/home_screen.dart
+++ lib/View/MainScreens/home_screen.dart
@@ -1,9 +1,14 @@
+import 'package:farmer_app/Utils/utils.dart';
+import 'package:farmer_app/Utils/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: CustomNavigationBar(
+        screen: homeScreen,
+      ),
       body: Center(
         child: Text("BINGO!!"),
       ),
