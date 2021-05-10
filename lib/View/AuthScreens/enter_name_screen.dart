9685ea24442138d72cec00d1ab3634cb83a4a54b
smile <imsmilegarg@gmail.com>
2021-05-10 17:54:42 +0530

    enter_name_screen.dart

--- lib/View/AuthScreens/enter_name_screen.dart
+++ lib/View/AuthScreens/enter_name_screen.dart
@@ -1,4 +1,5 @@
 import 'package:farmer_app/Utils/button.dart';
+import 'package:farmer_app/Utils/constants.dart';
 import 'package:farmer_app/Utils/utils.dart';
 import 'package:farmer_app/Utils/widgets.dart';
 import 'package:farmer_app/View/MainScreens/home_screen.dart';
@@ -10,17 +11,27 @@ class EnterNameScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar("Enter Your Name"),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          TextField(
-            decoration: inputDecor(hint: "Enter Your Name"),
-          ),
-          CustomButton(
-            child: Text("Get Started"),
-            onPressed: () => Get.to(() => HomeScreen()),
-          ),
-        ],
+      body: Padding(
+        padding: sidePadding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextField(
+              decoration: inputDecor(hint: "Enter Your Name"),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CustomButton(
+                  child: Text("Get Started"),
+                  onPressed: () => Get.to(() => HomeScreen()),
+                ),
+                verSpacing5,
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
