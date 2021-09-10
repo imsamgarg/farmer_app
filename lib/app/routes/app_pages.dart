b9c0251c46d0c5df96b3221fcc91fe7877386a40
Smile Garg <smlgarg123@gmail.com>
2021-09-10 20:56:17 +0530

    app pages refactor

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -5,18 +5,13 @@ import 'package:farmer_app/app/modules/crop_manual/views/crop_manual_view.dart';
 import 'package:farmer_app/app/modules/disease_detection/bindings/disease_detection_binding.dart';
 import 'package:farmer_app/app/modules/disease_detection/views/disease_detection_view.dart';
 import 'package:farmer_app/app/modules/home/bindings/home_binding.dart';
-import 'package:farmer_app/app/modules/home/views/create_post_view.dart';
 import 'package:farmer_app/app/modules/home/views/home_view.dart';
-import 'package:farmer_app/app/modules/home/views/notifications_view.dart';
 import 'package:farmer_app/app/modules/launch_screen/bindings/launch_screen_binding.dart';
 import 'package:farmer_app/app/modules/launch_screen/views/welcome_view.dart';
 import 'package:farmer_app/app/modules/login/bindings/login_binding.dart';
 import 'package:farmer_app/app/modules/login/views/login_view.dart';
 import 'package:farmer_app/app/modules/paddy_straw_management/bindings/paddy_straw_management_binding.dart';
 import 'package:farmer_app/app/modules/paddy_straw_management/views/paddy_straw_management_view.dart';
-import 'package:farmer_app/app/modules/profile/bindings/profile_binding.dart';
-import 'package:farmer_app/app/modules/profile/views/edit_profile_view.dart';
-import 'package:farmer_app/app/modules/profile/views/profile_view.dart';
 import 'package:farmer_app/app/modules/register/bindings/register_binding.dart';
 import 'package:farmer_app/app/modules/register/views/register_view.dart';
 import 'package:farmer_app/app/modules/startup/bindings/startup_binding.dart';
@@ -37,16 +32,6 @@ class AppPages {
       page: () => HomeView(),
       binding: HomeBinding(),
     ),
-    GetPage(
-      name: _Paths.CREATE_POST,
-      page: () => CreatePostView(),
-      binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.NOTIFICATIONS,
-      page: () => NotificationsView(),
-      binding: HomeBinding(),
-    ),
 
     ///Welcome Screen
     GetPage(
@@ -55,17 +40,6 @@ class AppPages {
       binding: LaunchScreenBinding(),
     ),
 
-    ///PROFILE
-    GetPage(
-      name: _Paths.PROFILE,
-      page: () => ProfileView(),
-      binding: ProfileBinding(),
-    ),
-    GetPage(
-      name: _Paths.PROFILE,
-      page: () => EditProfileView(),
-      binding: ProfileBinding(),
-    ),
     GetPage(
       name: _Paths.WEATHER_REPORT,
       page: () => WeatherReportView(),
