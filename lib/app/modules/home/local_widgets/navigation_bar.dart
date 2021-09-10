820dd0effcf62a828bf3cfa5bc56392fe44c6b5d
Smile Garg <smlgarg123@gmail.com>
2021-09-10 21:05:23 +0530

    new var bar created

---
+++ lib/app/modules/home/local_widgets/navigation_bar.dart
@@ -0,0 +1,30 @@
+import 'package:farmer_app/app/modules/home/controllers/home_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+
+class CustomBottomNavBar extends GetView<HomeController> {
+  const CustomBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: controller.pageIndex,
+      onTap: controller.onPageChange,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_rounded),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_circle_outline_rounded),
+          label: "Post",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle_rounded),
+          label: "Profile",
+        ),
+      ],
+      selectedItemColor: Theme.of(context).colorScheme.primary,
+    );
+  }
+}
