2e819d40b9a49e724955983775fa76fef95a8cf2
Smile Garg <smlgarg123@gmail.com>
2021-09-10 20:56:04 +0530

    profile page removed

--- lib/app/modules/profile/controllers/profile_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:farmer_app/app/routes/app_pages.dart';
-import 'package:get/get.dart';
-
-class ProfileController extends GetxController {
-  void editProfile() {
-    Get.toNamed(Routes.EDIT_PROFILE);
-  }
-}
