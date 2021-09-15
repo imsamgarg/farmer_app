ad15d7badd4216baa92761d5469a4309e846327c
Smile Garg <smlgarg123@gmail.com>
2021-09-16 00:07:34 +0530

    profile controller added to bindings

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,3 +1,4 @@
+import 'package:farmer_app/app/modules/home/controllers/profile_controller.dart';
 import 'package:get/get.dart';
 
 import 'package:farmer_app/app/modules/home/controllers/notification_controller.dart';
@@ -7,11 +8,14 @@ import '../controllers/home_controller.dart';
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
     Get.lazyPut<NotificationController>(
       () => NotificationController(),
     );
-    Get.lazyPut<HomeController>(
-      () => HomeController(),
+    Get.lazyPut<ProfileController>(
+      () => ProfileController(),
     );
   }
 }
