3ee2c7d034a9ec6d37735724f0fdc3665c232dfa
Smile Garg <smlgarg123@gmail.com>
2021-09-16 00:04:50 +0530

    assets file added

---
+++ lib/assets.dart
@@ -0,0 +1,35 @@
+class Assets {
+  Assets._();
+
+  static final misc = _AssetsMisc._();
+  static final promo = _AssetsPromo._();
+  static final to = Assets._();
+  final cropPNG = 'assets/crop.png';
+  final cropManualPNG = 'assets/crop_manual.png';
+  final diseasePNG = 'assets/disease.png';
+  final diseaseFullPNG = 'assets/disease_full.png';
+  final fadeSunPNG = 'assets/fade_sun.png';
+  final feedimageJPG = 'assets/feedimage.jpg';
+  final logoPNG = 'assets/logo.png';
+  final logoSmPNG = 'assets/logoSm.png';
+  final medicPNG = 'assets/medic.png';
+  final paddyStrawPNG = 'assets/paddy_straw.png';
+  final paddyStrawFulPNG = 'assets/paddy_straw_ful.png';
+  final splashPNG = 'assets/splash.png';
+  final sunImagePNG = 'assets/sun_image.png';
+}
+
+class _AssetsMisc {
+  _AssetsMisc._();
+
+  final weatherReportPNG = 'assets/Misc/weatherReport.png';
+}
+
+class _AssetsPromo {
+  _AssetsPromo._();
+
+  final pm1JPG = 'assets/promo/pm1.jpg';
+  final pm2JPG = 'assets/promo/pm2.jpg';
+  final pm3JPG = 'assets/promo/pm3.jpg';
+  final pm4JPG = 'assets/promo/pm4.jpg';
+}
