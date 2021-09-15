bd5e1e17295c19de0c9e8f9c4ae9a88f375a2e09
Smile Garg <smlgarg123@gmail.com>
2021-09-16 00:09:21 +0530

    routes added to profile view

--- lib/app/modules/home/controllers/profile_controller.dart
+++ lib/app/modules/home/controllers/profile_controller.dart
@@ -1,3 +1,4 @@
+import 'package:farmer_app/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 import 'package:farmer_app/app/modules/home/views/edit_profile_view.dart';
@@ -5,5 +6,25 @@ import 'package:farmer_app/app/modules/home/views/edit_profile_view.dart';
 class ProfileController extends GetxController {
   void editProfile() {
     Get.to(EditProfileView());
+  }
+
+  void openWeatherReport() {
+    Get.toNamed(Routes.WEATHER_REPORT);
+  }
+
+  void openDiseaseDetection() {
+    Get.toNamed(Routes.DISEASE_DETECTION);
+  }
+
+  void openCropManual() {
+    Get.toNamed(Routes.CROP_MANUAL);
+  }
+
+  void openBuyInput() {
+    Get.toNamed(Routes.BUY_INPUT);
+  }
+
+  void openSellProduce() {
+    Get.toNamed(Routes.SELL_PRODUCE);
   }
 }
