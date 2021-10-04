2a256614619b544addafc4475016f5805edda9f6
Smile Garg <smlgarg123@gmail.com>
2021-10-04 23:14:04 +0530

    validator mixin added

---
+++ lib/app/core/utils/mixins.dart
@@ -0,0 +1,13 @@
+mixin Validators {
+  String? phoneNumberValidator(String? value, [int minLen = 10]) {
+    return null;
+  }
+
+  String? otpCodeValidator(String? value, [int minLen = 6]) {
+    return null;
+  }
+
+  String? nameValidotor(String? value, [bool numbersAllowed = false]) {
+    return null;
+  }
+}
