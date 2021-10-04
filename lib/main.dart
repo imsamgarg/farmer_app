39f4962bb852afb2cbe3999566584dfe30217517
Smile Garg <smlgarg123@gmail.com>
2021-10-04 23:22:42 +0530

    firebase app initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,7 +7,9 @@ import 'package:farmer_app/app/core/theme/app_theme.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
