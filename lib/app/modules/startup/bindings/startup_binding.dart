99254c18abdfe1afde247c3f6d241065f2697ce7
Smile Garg <smlgarg123@gmail.com>
2021-10-09 18:00:51 +0530

    services added to startup

--- lib/app/modules/startup/bindings/startup_binding.dart
+++ lib/app/modules/startup/bindings/startup_binding.dart
@@ -1,4 +1,6 @@
 import 'package:farmer_app/app/data/services/auth_service.dart';
+import 'package:farmer_app/app/data/services/db_service.dart';
+import 'package:farmer_app/app/data/services/storage_service.dart';
 import 'package:get/get.dart';
 
 import '../controllers/startup_controller.dart';
@@ -7,6 +9,8 @@ class StartupBinding extends Bindings {
   @override
   void dependencies() {
     Get.put<AuthService>(AuthService());
+    Get.put<DatabaseService>(DatabaseService());
+    Get.put<StorageService>(StorageService());
     Get.put<StartupController>(StartupController());
   }
 }
