a26f889d5060b99aef1942afe86ab2fd97c521d7
Smile Garg <smlgarg123@gmail.com>
2021-10-09 18:03:08 +0530

    validators done

--- lib/app/core/utils/mixins.dart
+++ lib/app/core/utils/mixins.dart
@@ -1,8 +1,11 @@
+import 'package:custom_utils/log_utils.dart';
 import 'package:get/utils.dart';
 
 mixin Validators {
   String? phoneNumberValidator(String? value, [int minLen = 10]) {
     value ??= "";
+    value.removeAllWhitespace;
+
     if (value.isEmpty) return "Please Enter Phone Number";
     if (!value.isPhoneNumber) return "Enter Valid Number";
     if (value.length < 10) return "Enter Valid Number";
@@ -11,6 +14,8 @@ mixin Validators {
 
   String? otpCodeValidator(String? value, [int minLen = 6]) {
     value ??= "";
+    value.removeAllWhitespace;
+
     if (value.isEmpty) return "Please Enter Otp";
     if (!value.isNumericOnly) return "Only Digits Are Allowed";
     if (value.length != minLen) return "Enter 6 Digits ";
@@ -19,7 +24,9 @@ mixin Validators {
 
   String? nameValidotor(String? value, [bool numbersAllowed = false]) {
     value ??= "";
+    value = value.removeAllWhitespace;
     if (value.isEmpty) return "Please Enter Name";
+    customLog(value);
     if (!value.isAlphabetOnly && !numbersAllowed) {
       return "Only Alphabets Are Allowed";
     }
