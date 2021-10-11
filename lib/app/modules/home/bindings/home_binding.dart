f6ebe86d7815127fb0b449a0ecc9ee0985b41e6d
Smile Garg <smlgarg123@gmail.com>
2021-10-11 10:54:04 +0530

    bindings added

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,13 +1,21 @@
-import 'package:farmer_app/app/modules/home/controllers/profile_controller.dart';
 import 'package:get/get.dart';
 
+import 'package:farmer_app/app/modules/home/controllers/create_post_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/notification_controller.dart';
+import 'package:farmer_app/app/modules/home/controllers/profile_controller.dart';
+import 'package:farmer_app/app/modules/home/controllers/profile_route_controller.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<CreatePostController>(
+      () => CreatePostController(),
+    );
+    Get.lazyPut<ProfileRoutesController>(
+      () => ProfileRoutesController(),
+    );
     Get.lazyPut<HomeController>(
       () => HomeController(),
     );
