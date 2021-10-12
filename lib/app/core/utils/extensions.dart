a18eaf3aeb862f6ed614e606de53bcc83a37134e
Smile Garg <smlgarg123@gmail.com>
2021-10-12 13:10:40 +0530

    short image name extension added

--- lib/app/core/utils/extensions.dart
+++ lib/app/core/utils/extensions.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
 
 extension ColorExtension on BuildContext {
   get primaryColor => Theme.of(this).primaryColor;
+}
+
+extension StringExtensions on String {
+  // String limitText([int len=10]){
+  //   return this.
+  // }
+  String get shortImageName {
+    final ext = p.extension(this);
+    final image = p.basenameWithoutExtension(this);
+    if (image.length < 10) {
+      return "$image$ext";
+    }
+
+    return "${image.substring(0, 10)}$ext";
+  }
 }
