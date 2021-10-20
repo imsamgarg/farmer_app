9bf75c79be02d1a3744e1a39c190036e02015ffe
Smile Garg <smlgarg123@gmail.com>
2021-10-20 14:08:03 +0530

    bindings created

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,17 +1,29 @@
 import 'package:get/get.dart';
 
+import 'package:farmer_app/app/modules/home/controllers/community_posts_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/create_post_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/feed_controller.dart';
+import 'package:farmer_app/app/modules/home/controllers/my_posts_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/notification_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/profile_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/profile_route_controller.dart';
 import 'package:farmer_app/app/modules/home/controllers/user_controller.dart';
+import 'package:farmer_app/app/modules/home/controllers/yeomen_posts_controller.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<YeomenPostsController>(
+      () => YeomenPostsController(),
+    );
+    Get.lazyPut<MyPostsController>(
+      () => MyPostsController(),
+    );
+    Get.lazyPut<CommunityPostsController>(
+      () => CommunityPostsController(),
+    );
     Get.lazyPut<FeedController>(
       () => FeedController(),
     );
