5c40cf3e48a4dc39c7d5b29ccfc054140242c36f
Smile Garg <smlgarg123@gmail.com>
2021-10-21 15:40:10 +0530

    my post view done

--- lib/app/modules/home/controllers/my_posts_controller.dart
+++ lib/app/modules/home/controllers/my_posts_controller.dart
@@ -1,19 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:farmer_app/app/core/utils/helper.dart';
 import 'package:farmer_app/app/data/models/post_model.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class MyPostsController extends GetxController {
-  late final scrollController = ScrollController();
-  late final instance = _getInitialData();
-  late final List<Post> posts;
-  int get postsCount => posts.length;
+  late final pagingController = PagingController<int, Post>(firstPageKey: 0)
+    ..addPageRequestListener((pageKey) {
+      _fetchPosts(pageKey);
+    });
+
+  static const int fetchCount = 20;
+
+  // late final instance = _getInitialData();
+  // late final List<Post> posts;
+  late final db = getDbService();
+  late final QuerySnapshot<Object?> postsSnapshots;
+  // int get postsCount => posts.length;
   late final user = getAuth().currentUser;
   final String listViewId = "list-view";
 
-  Future<bool> _getInitialData() async {
-    final snapshots = await getDbService().getPosts(uid: user!.uid, count: 20);
-    posts = snapshots.docs.map((e) => Post.fromJson(e.data()!)).toList();
-    return true;
+  // Future<bool> _getInitialData() async {
+  //   final snapshots = await db.getPosts(uid: user!.uid, count: fetchCount);
+  //   postsSnapshots = snapshots;
+  //   // posts = snapshots.docs.map((e) => Post.fromJson(e.data()!)).toList();
+  //   return true;
+  // }
+
+  Future<void> _fetchPosts(int pageKey) async {
+    final snapshots = await db.getPosts(
+      uid: user!.uid,
+      count: fetchCount,
+      startAfter: pageKey == 0 ? null : postsSnapshots.docs.last,
+    );
+    final posts = snapshots.docs.map((e) => Post.fromJson(e.data()!)).toList();
+    final isLast = snapshots.docs.length < fetchCount;
+    if (isLast) {
+      pagingController.appendLastPage(posts);
+    } else {
+      pagingController.appendPage(posts, pageKey + posts.length);
+    }
   }
 }
--- lib/app/modules/home/views/my_posts_view.dart
+++ lib/app/modules/home/views/my_posts_view.dart
@@ -1,32 +1,21 @@
-import 'package:custom_utils/future.dart';
-import 'package:farmer_app/app/core/global_widgets/widgets.dart';
+import 'package:farmer_app/app/data/models/post_model.dart';
 import 'package:farmer_app/app/modules/home/controllers/my_posts_controller.dart';
 import 'package:farmer_app/app/modules/home/local_widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class MyPostsView extends GetView<MyPostsController> {
   @override
   Widget build(BuildContext context) {
-    return CustomFutureBuilder(
-      loading: CenterLoading(),
-      future: controller.instance,
-      builder: (context) {
-        return GetBuilder(
-          id: controller.listViewId,
-          init: controller,
-          builder: (_) {
-            return ListView.builder(
-              controller: controller.scrollController,
-              itemCount: controller.postsCount,
-              itemBuilder: (c, i) {
-                return FeedPost(post: controller.posts[i]);
-              },
-            );
-          },
-        );
-      },
+    return PagedListView(
+      pagingController: controller.pagingController,
+      builderDelegate: PagedChildBuilderDelegate<Post>(
+        itemBuilder: (c, item, i) {
+          return FeedPost(post: item);
+        },
+      ),
     );
   }
 }
