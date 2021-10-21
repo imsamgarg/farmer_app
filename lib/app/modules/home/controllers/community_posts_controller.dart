641c4bf5a9359310d464c42d2596291dc50b49d7
Smile Garg <smlgarg123@gmail.com>
2021-10-21 15:40:40 +0530

    my post copied to community post

--- lib/app/modules/home/controllers/community_posts_controller.dart
+++ lib/app/modules/home/controllers/community_posts_controller.dart
@@ -1,8 +1,32 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farmer_app/app/core/utils/helper.dart';
+import 'package:farmer_app/app/data/models/post_model.dart';
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class CommunityPostsController extends GetxController {
-  late final scrollController = ScrollController();
+  late final pagingController = PagingController<int, Post>(firstPageKey: 0)
+    ..addPageRequestListener((pageKey) {
+      _fetchPosts(pageKey);
+    });
 
+  static const int fetchCount = 20;
+  late final db = getDbService();
+  late final QuerySnapshot<Object?> postsSnapshots;
+  late final user = getAuth().currentUser;
   final String listViewId = "list-view";
+
+  Future<void> _fetchPosts(int pageKey) async {
+    final snapshots = await db.getPosts(
+      count: fetchCount,
+      startAfter: pageKey == 0 ? null : postsSnapshots.docs.last,
+    );
+    final posts = snapshots.docs.map((e) => Post.fromJson(e.data()!)).toList();
+    final isLast = snapshots.docs.length < fetchCount;
+    if (isLast) {
+      pagingController.appendLastPage(posts);
+    } else {
+      pagingController.appendPage(posts, pageKey + posts.length);
+    }
+  }
 }
--- lib/app/modules/home/views/community_posts_view.dart
+++ lib/app/modules/home/views/community_posts_view.dart
@@ -1,22 +1,21 @@
+import 'package:farmer_app/app/data/models/post_model.dart';
 import 'package:farmer_app/app/modules/home/controllers/community_posts_controller.dart';
+import 'package:farmer_app/app/modules/home/local_widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class CommunityPostsView extends GetView<CommunityPostsController> {
   @override
   Widget build(BuildContext context) {
-    return GetBuilder(
-      id: controller.listViewId,
-      init: controller,
-      builder: (_) {
-        return ListView.builder(
-          controller: controller.scrollController,
-          itemBuilder: (c, i) {
-            return Container();
-          },
-        );
-      },
+    return PagedListView(
+      pagingController: controller.pagingController,
+      builderDelegate: PagedChildBuilderDelegate<Post>(
+        itemBuilder: (c, item, i) {
+          return FeedPost(post: item);
+        },
+      ),
     );
   }
 }
